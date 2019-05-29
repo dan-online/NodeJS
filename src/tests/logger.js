6b07448a9db4ef984887d9cc48f5f88906fdfbd8
MayorChano <dan@dancodes.online>
2019-05-29 06:55:15 +0200

    double-quotes

--- src/tests/logger.js
+++ src/tests/logger.js
@@ -1,25 +1,25 @@
-const chalk = require('chalk');
-const logged = [{name: 'Initialize', value: false}, {name: 'Listening', value: false}, {name: 'Server', value: false}, {name: 'Routes', value: false}, {name: "Database", value: false}]
-const config = require('../../package.json')
+const chalk = require("chalk");
+const logged = [{name: "Initialize", value: false}, {name: "Listening", value: false}, {name: "Server", value: false}, {name: "Routes", value: false}, {name: "Database", value: false}]
+const config = require("../../package.json")
 process.stdout.moveCursor(0, -1);
 process.stdout.clearLine();
 var test = false; 
-var word = 'Online';
+var word = "Online";
 
 function log(change) {
-    if(change == 'test') {
+    if(change == "test") {
         test = true;
-        word = 'OK';
+        word = "OK";
     }
-    if(config.log == 'tech') {
+    if(config.log == "tech") {
         if(logged.find(x => x.name.toLowerCase() == change)) logged.find(x => x.name.toLowerCase() == change).value = true;
         //logged.forEach(l => process.stdout.clearLine());
         logged.forEach(l => process.stdout.moveCursor(0, -1) && process.stdout.clearLine());
         logged.forEach(l => {
         if(l.value) {
-            process.stdout.clearLine(); process.stdout.write(chalk.black.bgGreen.bold(' ' + word + ' ') + ' ' + l.name + '\n');
+            process.stdout.clearLine(); process.stdout.write(chalk.black.bgGreen.bold(" " + word + " ") + " " + l.name + "\n");
         } else {
-            process.stdout.clearLine(); process.stdout.write(chalk.black.bgRed.bold(' Starting ') + ' ' + l.name + '\n');
+            process.stdout.clearLine(); process.stdout.write(chalk.black.bgRed.bold(" Starting ") + " " + l.name + "\n");
         }
         });
         if(logged.filter(x => !x.value).length == 0) {
