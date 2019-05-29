47535826b4ca54b15b3be4548a023c6b78df1069
MayorChano <dan@dancodes.online>
2019-05-29 07:05:59 +0200

    one parentheses fix

--- src/tests/logger.js
+++ src/tests/logger.js
@@ -21,14 +21,14 @@ function log(change) {
             logged.find((x) => x.name.toLowerCase() === change).value = true;
         }
         //logged.forEach(l => process.stdout.clearLine());
-        logged.forEach(l => {
-        process.stdout.moveCursor(0, -1);
-        process.stdout.clearLine();
-        if(l.value) {
-            process.stdout.clearLine(); process.stdout.write(chalk.black.bgGreen.bold(" " + word + " ") + " " + l.name + "\n");
-        } else {
-            process.stdout.clearLine(); process.stdout.write(chalk.black.bgRed.bold(" Starting ") + " " + l.name + "\n");
-        }
+        logged.forEach((l) => {
+            process.stdout.moveCursor(0, -1);
+            process.stdout.clearLine();
+            if(l.value) {
+                process.stdout.clearLine(); process.stdout.write(chalk.black.bgGreen.bold(" " + word + " ") + " " + l.name + "\n");
+            } else {
+                process.stdout.clearLine(); process.stdout.write(chalk.black.bgRed.bold(" Starting ") + " " + l.name + "\n");
+            }
         });
         if(logged.filter((x) => !x.value).length === 0) {
             if(test) {
