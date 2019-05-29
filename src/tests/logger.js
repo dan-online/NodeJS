9dd5135ca415c7572cb928c943e85e3c777cf285
MayorChano <dan@dancodes.online>
2019-05-29 06:52:14 +0200

    stuff

--- src/tests/logger.js
+++ src/tests/logger.js
@@ -3,26 +3,33 @@ const logged = [{name: 'Initialize', value: false}, {name: 'Listening', value: f
 const config = require('../../package.json')
 process.stdout.moveCursor(0, -1);
 process.stdout.clearLine();
+var test = false; 
+var word = 'Online';
 
 function log(change) {
+    if(change == 'test') {
+        test = true;
+        word = 'OK';
+    }
     if(config.log == 'tech') {
-        logged.find(x => x.name.toLowerCase() == change).value = true;
+        if(logged.find(x => x.name.toLowerCase() == change)) logged.find(x => x.name.toLowerCase() == change).value = true;
         //logged.forEach(l => process.stdout.clearLine());
         logged.forEach(l => process.stdout.moveCursor(0, -1) && process.stdout.clearLine());
         logged.forEach(l => {
         if(l.value) {
-            process.stdout.clearLine(); process.stdout.write(chalk.black.bgGreen.bold(' Online ') + ' ' + l.name + '\n');
+            process.stdout.clearLine(); process.stdout.write(chalk.black.bgGreen.bold(' ' + word + ' ') + ' ' + l.name + '\n');
         } else {
             process.stdout.clearLine(); process.stdout.write(chalk.black.bgRed.bold(' Starting ') + ' ' + l.name + '\n');
         }
         });
         if(logged.filter(x => !x.value).length == 0) {
+            if(test) {
+                return process.exit(0);
+            }
             process.stdout.write(`\n${config.name}-${config.version} listening on port ${config.port}\n`);
         }
     } else {
-        if(change == 'server') {
-            process.stdout.write(`\n${config.name}-${config.version} listening on port ${config.port}\n`);
-        }
+        process.stdout.write(`\n${config.name}-${config.version} listening on port ${config.port}\n`);
     }
 }
 module.exports = log;
