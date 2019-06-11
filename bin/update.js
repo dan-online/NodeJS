374fbe08342d58f25255c426ef07435b21f81c2b
MayorChano <dan@dancodes.online>
2019-06-11 12:44:55 +0200

    double quotes

--- bin/update.js
+++ bin/update.js
@@ -1,40 +1,40 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 const inquirer = require("inquirer");
 const log = require("../src/tests/logger");
 
 async function run(code) {
-	var r = await exec(code).catch(err => {if(err) var r = {type: 'error', stderr:err}}
+	var r = await exec(code).catch(err => {if(err) var r = {type: "error", stderr:err}}
 );
 	if(!r) r = {stderr: r}
 	if (r.stderr) {
-    	return {type: 'error', output: r.stderr};
+    	return {type: "error", output: r.stderr};
   	} else {
-  		return {type: 'success', output: r.stdout};
+  		return {type: "success", output: r.stdout};
   	}
 }
 
 module.exports.update = async function () {
-    await run('git fetch github');
-    await run('git remote add github https://github.com/MayorChano/NodeJS');
+    await run("git fetch github");
+    await run("git remote add github https://github.com/MayorChano/NodeJS");
     async function file(file) {
-        let check = await run('git diff github/master --name-only ' + file)
-        if(check.output || check.type == 'error') {
+        let check = await run("git diff github/master --name-only " + file)
+        if(check.output || check.type == "error") {
             var questions = [{
                     type: "list",
                     name: "update",
-                    message: "Would you like to update " + file + '?',
+                    message: "Would you like to update " + file + "?",
                     choices: ["Yes", "No"],
                 }];
             const answers = await inquirer.prompt(questions)
-            if(answers['update'] == 'Yes') {
-                await run('git checkout github/master ' + file);
+            if(answers["update"] == "Yes") {
+                await run("git checkout github/master " + file);
             }
         }
     }
 
-    await file('./bin/www');
-    await file('./bin/update.js');
+    await file("./bin/www");
+    await file("./bin/update.js");
     log("update")
-    //await run('git checkout origin/master ' + __dirname + '/bin/www')
+    //await run("git checkout origin/master " + __dirname + "/bin/www")
 }
