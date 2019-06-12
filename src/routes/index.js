9c7564f667ce937015332c17667bb7c855a4c30c
MayorChano <dan@dancodes.online>
2019-06-12 12:29:57 +0200

    usage

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const os = require("os-utils");
 
 router.get("/", (req, res) => {
-  res.render("index");
+  const cpu = os.cpuUsage(function(cpu) {
+    res.render("index", {cpu});
+  });
 });
 
 module.exports = router;
